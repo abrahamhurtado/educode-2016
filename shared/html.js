b12534b738474cde57a77590717413e9ce6104dc
arhcstolen <arhc.hc@gmail.com>
2016-03-03 14:50:34 -0700

    Materialize.css

--- shared/html.js
+++ shared/html.js
@@ -6,10 +6,12 @@ export default (Component) => `
       <meta name="viewport" content="initial-scale=1, maximum-scale=1" />
       <title>My React Boilerplate</title>
       <link rel="stylesheet" href="/static/style.css" >
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/css/materialize.min.css">
     </head>
     <body>
       <div id="react-app">${Component}</div>
       <script type="text/javascript" src="/static/bundle.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/js/materialize.min.js"></script>
     </body>
   </html>
 `
