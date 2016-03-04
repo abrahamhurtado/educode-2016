1dc8877d65240013f00ef069ab639572b7c27245
ramonramirezz <ramon.ramirezp@outlook.com>
2016-03-03 18:22:16 -0700

    conflicts ready

--- shared/routes.jsx
+++ shared/routes.jsx
@@ -6,7 +6,7 @@ import Children from './components/Children';
 import Register from './components/Register'
 import CrearVideo from './components/crearVideo.jsx';
 import CrearTexto from './components/crearText.jsx';
->>>>>>> ac12c7ef09bbe35a99dd18d0144a12e07b147d46
+
 
 export default {
   path: '/',
@@ -14,7 +14,7 @@ export default {
   indexRoute: { component: Home },
   childRoutes: [
     { path: '/children', component: Children },
-    {path:'/register', component:Register}
+    {path:'/register', component:Register},
     { path: '/create/video', component: CrearVideo },
     { path: '/create/text', component: CrearTexto }
   ]
