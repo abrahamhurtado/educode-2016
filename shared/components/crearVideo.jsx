93e0c5e48a0ff82c3af88b6af3995572d2b51055
Juanets <jdgs.gt@gmail.com>
2016-03-04 04:56:49 -0700

    cambios a diseño de plantillas y agregado ayuda de markup

--- shared/components/crearVideo.jsx
+++ shared/components/crearVideo.jsx
@@ -3,28 +3,48 @@ import React from 'react';
 export default class CrearVideo extends React.Component {
   render () {
     return (
-      <div className="card">
-        <div className="card-content">
-          <h3>Ingresa la información del video</h3>
-          <form>
-            <div className="input-field">
-              <label>Título de la lección</label>
-              <input className="validate" id="titulo" required type="text" name="titulo" placeholder="Título de la lección"></input>
+      <div className="row">
+
+          <form className="col s8 offset-s4">
+
+          <div className="row">
+              <div className=" header col s8">
+                    
+                    <h3>Ingresa la información del video</h3>
+
+              </div>
+          </div>
+
+          <div className="row">          
+            <div className="input-field col s6">
+              <input className="validate" id="titulo" required type="text"></input>
+              <label className="validate" for="titulo">Título de la lección</label>  
             </div>
-            <p></p>
-            <div className="input-field">
-              <label>Descripción de la lección</label>
-              <textarea className="materialize-textarea" required name="descripcion" placeholder="Descripción de la lección"/>
+          </div>
+            
+
+          <div className="row">          
+            <div className="input-field col s6">
+              <input className="validate" id="titulo" required type="text"></input>
+              <label className="validate" for="titulo">Descripción de la lección</label>  
             </div>
-            <div className="input-field">
-              <label>Dirección del video</label>
-              <input required type="url" placeholder="Un vídeo de YouTube, por ejemplo"></input>
+          </div>
+
+          <div className="row">          
+            <div className="input-field col s6">
+              <input className="validate" id="titulo" required type="url"></input>
+              <label className="validate" for="titulo">Enlace del video</label>  
             </div>
+          </div>
+            <div className="row">
+        <div className="col s6 ">
             <button className="waves-effect waves-light btn" type="submit">Guardar lección</button>
-            <button className="btn-floating btn-large waves-effect waves-light red right"><i className="material-icons">add</i></button>
-        </form>
+            <button className="btn-floating btn-medium waves-effect waves-light red right"><i className="material-icons">add</i></button>
         </div>
       </div>
+        </form>
+        </div>
+    
     );
   }
 }
