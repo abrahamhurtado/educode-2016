98e654f5e98645ca1984dcf26adacba3e69ed41b
Juanets <jdgs.gt@gmail.com>
2016-03-03 18:06:41 -0700

    navbar actualizado

--- shared/html.js
+++ shared/html.js
@@ -5,12 +5,14 @@ export default (Component) => `
       <meta charSet="utf-8"/>
       <meta name="viewport" content="initial-scale=1, maximum-scale=1" />
       <title>My React Boilerplate</title>
-      <link rel="stylesheet" href="/static/style.css" >
+      <link rel="stylesheet" type="text/css" href="/static/style.css" >
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/css/materialize.min.css">
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     </head>
     <body>
       <div id="react-app">${Component}</div>
       <script type="text/javascript" src="/static/bundle.js"></script>
+      <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/js/materialize.min.js"></script>
     </body>
   </html>
